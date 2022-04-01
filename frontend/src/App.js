8a240febc959683ae54188ff5f5f65f2e024246b
Sarthakischauhan <sarthakchauhann@gmail.com>
2022-04-01 02:33:00 -0700

    added static popup functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import { useState } from "react";
-import Map, {Marker} from 'react-map-gl';
-import { Room } from '@material-ui/icons';
+import Map, {Marker, Popup} from 'react-map-gl';
+import { Room, Star} from '@material-ui/icons';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
     latitude: 37.31512016548463,
     zoom:4
   })
+  const [showPopup,setShowPopup] = useState(true);
   return (
     <Map
       onMove={ evt => setViewport(evt.viewState)}
@@ -22,6 +23,26 @@ const App = () => {
       <Marker longitude={-121.78349704449576} latitude={37.31512016548463} anchor="bottom" >
           <Room style={{fontSize : viewport.zoom * 8 ,color:"slateblue"}} />
       </Marker> 
+      <Popup longitude={-121.78349704449576} latitude={37.31512016548463}
+        anchor="right"
+        onClose={() => setShowPopup(false)}>
+        <div className='card'>
+          <label>Place</label>
+          <h4 className='place'>Home</h4>
+          <label>Experience</label>
+          <p className='exp'>It feels like home.</p>
+          <label>Mood</label>
+          <div className="mood">
+            <Star className='star'/>
+            <Star className='star'/> 
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+          </div>
+          <label>Created At</label>
+          <h4 className='time'>1 hour ago</h4>
+        </div>
+      </Popup>
     </Map>
   );
 }
