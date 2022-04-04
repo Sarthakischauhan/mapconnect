c3b6024eb54981d24b6e569556f46056a4bddfba
Sarthakischauhan <sarthakchauhann@gmail.com>
2022-04-04 04:28:32 -0700

    add location feature added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import pinRouter from "./routes/pin.js"
 import userRouter from "./routes/users.js"
+
 
 const app = express();
 dotenv.config();
@@ -13,7 +15,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-
+app.use(cors());
 mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.kgfxb.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`,{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log("Mongo db was connected ")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useEffect } from 'react';
 import { useState } from "react";
 import Map, {Marker, Popup} from 'react-map-gl';
-import { Room, Star} from '@material-ui/icons';
+import { FlipRounded, Room, Star} from '@material-ui/icons';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import axios from "axios";
 import {format} from "timeago.js";
@@ -10,12 +10,16 @@ import {format} from "timeago.js";
 
 const App = () => {
   const [pins,setPins] = useState([]);
+  const [currentUser,setCurrentUser] = useState("sarthak999")
   const [viewport,setViewport] = useState({
     longitude: 78.8718, 
     latitude: 21.7679,
     zoom:4
   })
-
+  const [newPlace,setNewPlace] = useState(null);
+  const [title,setTitle] = useState("");
+  const [desc,setDesc] = useState("");
+  const [rating,setRating] = useState(null);
   const [showPopup,setShowPopup] = useState(true);
   const [currentPlaceId,setCurrentPlaceId] = useState(null);
 
@@ -24,10 +28,38 @@ const App = () => {
       setViewport({...viewport,latitude:lat,longitude:long});
   }
 
+  const handleClickAdd = (e) => {
+      const {lng : longitude, lat : latitude} = e.lngLat;
+      setNewPlace({
+        lat:latitude,
+        long:longitude
+      });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username : currentUser,
+      title:title,
+      desc:desc,
+      rating:rating,
+      lat:newPlace.lat,
+      long:newPlace.long
+    };
+
+    try {
+      const allPins = await axios.post("http://localhost:8800/api/pin",newPin);
+      setPins([...pins,allPins.data]);
+      setNewPlace(null);
+    }catch(err){
+      console.log(err)
+    }
+
+  }
   useEffect( () => {
     const getPins = async () => {
       try{
-        const res = await axios.get("http://localhost:8800/api/pin");
+        const res = await axios.get("http://localho.st:8800/api/pin");
         setPins(res.data)
       }
       catch (err) {
@@ -44,12 +76,13 @@ const App = () => {
       style={{width: "100%", height: "100vh"}}
       mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY}
       mapStyle="mapbox://styles/mapbox/streets-v9"
+      onDblClick={handleClickAdd}
     >
       {pins.map(pin => {
         return (
           <>
             <Marker longitude={pin.long} latitude={pin.lat} anchor="bottom" >
-              <Room style={{fontSize : viewport.zoom * 8 ,color:"slateblue",cursor:"pointer"}} onClick={() => handleMarkerCLick(pin._id,pin.lat,pin.long)} />
+              <Room style={{fontSize : viewport.zoom < 10 ? 45 : 35 ,color:currentUser === pin.username ? "orange" : "slateblue",cursor:"pointer"}} onClick={() => handleMarkerCLick(pin._id,pin.lat,pin.long)} />
             </Marker> 
 
             {currentPlaceId === pin._id &&
@@ -68,9 +101,45 @@ const App = () => {
                 <div className="mood">
                     {Array(pin.rating).fill(<Star className="star" />)}
                 </div>
-                <label>Created At</label>
-                <h4 className='time'>{format(pin.createdAt)}</h4>
+                <label>Info</label>
+                <h4 className='user'>{`${pin.username} , ${format(pin.createdAt)}`}</h4>
               </div>
+              </Popup>
+            }
+            {newPlace &&
+              <Popup 
+              longitude={newPlace.long}
+              latitude={newPlace.lat}
+              anchor="right"
+              closeButton={true}
+              onClose={() => setNewPlace(null)}
+              >
+                <div>
+                  <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input
+                      placeholder="Enter a title"
+                      autoFocus
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <label>Description</label>
+                    <textarea
+                      placeholder="Say us something about this place."
+                      onChange={(e) => setDesc(e.target.value)}
+                    />
+                    <label>Rating</label>
+                    <select onChange={(e) => setRating(e.target.value)}>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                    <button type="submit" className="submitButton">
+                      Add Pin
+                    </button>
+                  </form>
+                </div>
               </Popup>
             }
           </>
